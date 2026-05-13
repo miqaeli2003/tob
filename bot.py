712dda7e41673a9feac6ab4170ff4aa25f022c95
miqaeli2003 <esebuamiqael@gmail.com>
2026-05-13 08:59:48 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
+# ----------------------------
+# HEADLESS CHROME SETUP
+# ----------------------------
 options = Options()
-
-options.add_argument("--headless=new")
+options.add_argument("--headless=new")          # no window
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
 
-options.binary_location = "/usr/bin/chromium"
-
-driver = webdriver.Chrome(
-service=Service(ChromeDriverManager().install()),
-options=options
-)
-
-driver.get("https://google.com")
-print(driver.title)
+driver = webdriver.Chrome(options=options)
 
 # Open site
 driver.get("http://vinme.ge/")
