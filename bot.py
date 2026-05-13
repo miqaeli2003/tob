e8f8093f20b6e33214b2a630038dbb62b48dc961
miqaeli2003 <esebuamiqael@gmail.com>
2026-05-13 08:45:15 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
 
-# ----------------------------
-# HEADLESS CHROME SETUP
-# ----------------------------
 options = Options()
-options.add_argument("--headless=new")          # no window
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1920,1080")
+
+options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=options)
 
